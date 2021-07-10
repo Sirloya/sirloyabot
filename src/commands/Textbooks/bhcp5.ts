7f3668ca6c45056bf8cd4120343b065e7c62a7fe
Sirloya <87189206+Sirloya@users.noreply.github.com>
2021-07-10 14:58:34 +0530

    Create bhcp5.ts

---
+++ src/commands/Textbooks/bhcp5.ts
@@ -0,0 +1,22 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import { MessageType } from '@adiwajshing/baileys'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'bhcp3',
+            description: 'Dwnload chp2',
+            aliases: ['chp2'],
+            category: 'textbooks',
+            usage: `${client.config.prefix}trigger [image | @mention]`
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        M.reply('https://drive.google.com/file/d/1VAvhliOxoIGLHf4iNoxxP8Yes051a5qO/view?usp=sharing \n\n\n Fajana paro ! Kanna Hotnaro ! \n\n           *Sir Loya*')
+
+    }
+}
